e3d531b962da76211bb4b368b462b69efc87b7c7
ThierryPSU <ign.thierry@gmail.com>
2026-06-17 11:47:24 +0700

    Update AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -16,7 +16,9 @@ class AuthController extends Controller
             'password' => 'required|string',
         ]);
 
-        $user = User::where('username', $request->username)->first();
+        $user = User::where('username', $request->username)
+                    ->orWhere('email', $request->username)
+                    ->first();
 
         if (!$user || !Hash::check($request->password, $user->password)) {
             return response()->json(['message' => 'Kredensial tidak valid'], 401);
