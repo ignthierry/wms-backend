ed92919c1635eda42de31557519e5ff4267009a4
ignthierry <ign.thierry@gmail.com>
2026-07-04 11:00:43 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -33,13 +33,14 @@ use App\Http\Controllers\ConfigurationController;
 Route::post('/auth/login', [AuthController::class, 'login']);
 Route::post('/auth/register', [AuthController::class, 'register']);
 
+Route::apiResource('roles', RoleController::class);
+Route::apiResource('users', UserController::class);
+
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
 
-    Route::apiResource('roles', RoleController::class);
-    Route::apiResource('users', UserController::class);
     Route::apiResource('clients', ClientController::class);
     Route::apiResource('warehouses', WarehouseController::class);
     Route::apiResource('locations', LocationController::class);
